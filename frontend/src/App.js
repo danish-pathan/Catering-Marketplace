59fd057df5d9993211367eac2d6b896beaff274f
danish-pathan <danishpathan3252@gmail.com>
2024-02-20 17:55:23 +0530

    Add: Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './components/Header'
 import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -8,9 +10,11 @@ const App = () => {
     <Header/>
     <main className='py-3'>
       <Container>
-        <h1>Welcome to Catering Marketplace</h1>
+        {/* <h1>Welcome to CaterPro! -  A smart choice for savvy catering solutions. 🍽️</h1> */}
+        <Outlet/>
       </Container>
     </main>
+    <Footer/>
     </>
   )
 } 
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import {LinkContainer} from 'react-router-bootstrap'
 
 const Header = () => {
   return (
-    <Header>
+    <header>
 
-    <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
+    <Navbar bg="dark" variant='dark' expand="md" collapseOnSelect>
         <Container>
-            <Navbar.Brand href="/">Catering Marketplace</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Catering Marketplace</Navbar.Brand>
+          </LinkContainer>
             <Navbar.Toggle aria-controls='basic-navbar-nav' />
             <Navbar.Collapse id='basic-navbar-nav'>
 
                 <Nav className='ms-auto'>
-                    <Nav.Link href='/cart'><FaShoppingCart /> Cart</Nav.Link>
-                    <Nav.Link href='/login'><FaUser /> Sign In</Nav.Link>
+                  <LinkContainer to="/cart">
+                    <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                  </LinkContainer>
+
+                  <LinkContainer to="login">
+                    <Nav.Link><FaUser /> Sign In</Nav.Link>
+                  </LinkContainer>
                 </Nav>
             </Navbar.Collapse>
         </Container>
     </Navbar>
-    </Header>
+    </header>
   )
 }
 
