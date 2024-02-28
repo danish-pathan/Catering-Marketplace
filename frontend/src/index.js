b932f3c2616e6fa2c1979adcbfb9f2463e5c65b6
danish-pathan <danishpathan3252@gmail.com>
2024-02-29 00:27:46 +0530

    feat: integrate PayPal payments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,11 +9,11 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
+import store from "./store";
 import "./assets/styles/bootstrap.custom.css";
 import "./assets/styles/index.css";
-
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
@@ -50,7 +50,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
